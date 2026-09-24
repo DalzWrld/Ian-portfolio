29aff3323721ae2908b0468e0e9d554e821bb086
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-09-24 10:55:33 +0300

    Add the imports in Footer

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,5 @@
+import { Linkedin, ArrowUpRight, Copyright } from "lucide-react"
+import { FaGithub, FaInstagram } from "react-icons/fa6"
+import { Link } from "react-router-dom"
+
+export function Footer() {}
