16597ae04ada7d65a73f00c302ccd54d4a51adae
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-09-24 15:59:18 +0300

    Add the imports in Work

---
+++ src/pages/Work.jsx
@@ -0,0 +1,6 @@
+import { SectionHeading } from "@/components/layout/SectionHeading"
+import { Reveal } from "@/components/Reveal"
+import { ProjectCard } from "@/components/sections/ProjectCard"
+import { useProjects } from "@/lib/projectStore"
+
+export default function Work() {}
