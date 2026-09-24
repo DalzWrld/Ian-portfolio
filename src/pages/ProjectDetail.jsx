236347647f0b3b241000e23757a5f4fa0aba21bb
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-09-24 15:44:08 +0300

    Add the imports in ProjectDetail

---
+++ src/pages/ProjectDetail.jsx
@@ -0,0 +1,9 @@
+import { Link, useParams } from "react-router-dom"
+import { ArrowLeft, ArrowUpRight } from "lucide-react"
+import { Badge } from "@/components/ui/badge"
+import { Button } from "@/components/ui/button"
+import { SectionHeading } from "@/components/layout/SectionHeading"
+import { Reveal } from "@/components/Reveal"
+import { getProject } from "@/lib/projectStore"
+
+export default function ProjectDetail() {}
